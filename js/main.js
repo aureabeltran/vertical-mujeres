8ee5c3052c2b29304e5a00f655d99bf186e343ef
aureabeltran <aureabeltranmanzo@gmail.com>
2016-07-25 11:10:02 -0400

    comentarios

--- js/main.js
+++ js/main.js
@@ -11,3 +11,28 @@ btnNuevo.click(function(){
 	 nuevo.show();
 	 popular.hide(1000);
 })
+//mensajes mientras
+
+var text = $("#mensaje");
+var user = $("#usuario");
+var chatPanel = $(".chat-panel");
+var newHour = new Date();
+	newHour.setTime(newHour.getTime());
+	var hora = new Date().getHours();
+	if (hora < 10) {
+		hora = "0" + hora;
+	}
+	var minuto = new Date().getMinutes();
+	if (minuto < 10) {
+		minuto = "0" + minuto;
+	}
+
+$(text).keypress(function( event ) {
+  if ( event.which == 13 ) {
+     	chatPanel.append('<div>' + "<h3>" + user.val() + ":" + "</h3>" + "<p class='texto'>" + text.val() + "</p>" + "<h5 class='texto'>" + hora + ":" + minuto + "</h5>" + '</div>');
+	 	text.val("");	
+	 	user.val("");
+    }
+    var chat  = $(".texto").parent();
+    chat.addClass("chat");
+});
